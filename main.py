efe81a8cb557c636dce62c2605e6d25362a8f09f
Zaur Bairamau <bairamau.z@gmail.com>
2025-05-27 22:09:19 +0400

    draw lines

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from window import Window
+from point import Point
+from line import Line
 
 def main():
     win = Window(800, 600)
+    
+    line1 = Line(Point(0, 0), Point(800, 600))
+    line2 = Line(Point(800, 0), Point(0, 600))
+    
+    win.draw_line(line1, "gold")
+    win.draw_line(line2, "lightblue")
+
     win.wait_for_close()
 
 
--- window.py
+++ window.py
@@ -5,7 +5,7 @@ class Window:
         self.root = Tk()
         self.root.title = 'Maze solver'
         
-        self.canvas = Canvas(self.root)
+        self.canvas = Canvas(self.root, width=width, height=height)
         self.canvas.pack()
         self.is_running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
@@ -18,8 +18,10 @@ class Window:
     def wait_for_close(self):
         self.is_running = True
         while self.is_running:
-            print(self.is_running)
             self.redraw()
 
     def close(self):
         self.is_running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
