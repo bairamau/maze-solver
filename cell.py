b5cef1433790880c40a0aefce857c982b88edaec
Zaur Bairamau <bairamau.z@gmail.com>
2025-05-28 04:46:58 +0400

    add cells

---
+++ cell.py
@@ -0,0 +1,45 @@
+from line import Line
+from point import Point
+
+
+class Cell:
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+        self.__x1 = -1
+        self.__x2 = -1
+        self.__y1 = -1
+        self.__y2 = -1
+
+        self.__win = window
+
+    def draw(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+
+        if self.has_left_wall:
+            self.__win.draw_line(
+                Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2)))
+
+        if self.has_top_wall:
+            self.__win.draw_line(
+                Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1)))
+
+        if self.has_right_wall:
+            self.__win.draw_line(
+                Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2)))
+        
+        if self.has_bottom_wall:
+            self.__win.draw_line(
+                Line(Point(self.__x2, self.__y2), Point(self.__x1, self.__y2)))
+        
+    def draw_move(self, to_cell, undo=False):
+        start = Point((self.__x1 + self.__x2) / 2, (self.__y1 + self.__y2) / 2)
+        end = Point((to_cell.__x1 + to_cell.__x2) / 2, (to_cell.__y1 + to_cell.__y2) / 2)
+
+        self.__win.draw_line(Line(start, end), 'gray' if undo else 'red')
--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 from window import Window
-from point import Point
-from line import Line
+# from point import Point
+# from line import Line
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    
-    line1 = Line(Point(0, 0), Point(800, 600))
-    line2 = Line(Point(800, 0), Point(0, 600))
-    
-    win.draw_line(line1, "gold")
-    win.draw_line(line2, "lightblue")
+
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
+
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c1.draw_move(c2)
+
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
 
     win.wait_for_close()
-
 
 if __name__ == "__main__":
     main()
